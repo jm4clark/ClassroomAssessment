e561b57a5c7ab57dea35576f26ae2770afac40a6
jm4clark <jamesmclark96@gmail.com>
2019-06-07 11:46:53 +0100

    Added methods to and tests for classroomMapRepo

--- src/main/java/com/bae/persistence/repository/ClassroomMapRepo.java
+++ src/main/java/com/bae/persistence/repository/ClassroomMapRepo.java
@@ -1,47 +1,56 @@
 package com.bae.persistence.repository;
 
-import static javax.transaction.Transactional.TxType.SUPPORTS;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.enterprise.inject.Default;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import javax.enterprise.inject.Alternative;
 
-@Transactional(SUPPORTS)
-@Default
+import com.bae.persistence.domain.Classroom;
+import com.bae.util.JSONUtil;
+
+
+@Alternative
 public class ClassroomMapRepo implements ClassroomRepository{
 
-	@PersistenceContext(unitName = "primary")
-	private EntityManager manager;
+	private JSONUtil util = new JSONUtil();
+	
+	Map<Integer, Classroom> classroomMap = new HashMap<>();
 	
 	@Override
 	public String createClassroom(String classroom) {
-		// TODO Auto-generated method stub
-		return null;
+		Classroom cr =util.getObjectForJSON(classroom, Classroom.class);
+		classroomMap.put(cr.getId(), cr);
+		return util.messageToJSON("Created account");
 	}
 
 	@Override
 	public String getAllClassrooms() {
-		// TODO Auto-generated method stub
-		return null;
+		String result = "";
+		for (Classroom c : classroomMap.values()) {
+			result += util.getJSONForObject(c) + "\n";
+		}
+		return result;
 	}
 
 	@Override
 	public String getAClassroom(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return util.getJSONForObject(classroomMap.get(id));
 	}
 
 	@Override
 	public String updateClassroom(int id, String classroom) {
-		// TODO Auto-generated method stub
-		return null;
+		Classroom newCR = util.getObjectForJSON(classroom, Classroom.class);
+		classroomMap.replace(id, newCR);
+		return util.messageToJSON("Updated account");
 	}
 
 	@Override
 	public String deleteClassroom(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		if (classroomMap.get(id) != null) {
+			classroomMap.remove(id);
+			return util.messageToJSON("Deleted classroom");
+		}
+		return util.messageToJSON("No account to delete");
 	}
 
 }
